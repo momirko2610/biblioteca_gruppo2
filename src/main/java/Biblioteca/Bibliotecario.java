07ec5711ed106c71237d030631b410818d1ff91f
Mirko Montella <m.montella25@studenti.unisa.it>
2025-12-08 19:02:45 +0100

    Doxygen wip completato

--- src/main/java/Biblioteca/Bibliotecario.java
+++ src/main/java/Biblioteca/Bibliotecario.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Biblioteca;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -17,14 +12,14 @@ import com.google.gson.GsonBuilder;
 public class Bibliotecario {
     private String e_mail; /*!<Email dell'account letta da una textbox che verrà confrontata con i dati presenti nel database dal metodo loginBibliotecario*/
     private String password; /*!<password dell'account letta da una textbox che verrà confrontata con i dati presenti nel database dal metodo loginBibliotecario*/
-    
+
     private static final String NAME = "database.json"; /*!<Nome del database da cui verranno confrontati i dati*/
-    
+
     private static final Gson database = new GsonBuilder().setPrettyPrinting().create();
     /**
      * @brief Costruttore di base
-     * @param e_mail L'email del bibliotecario
-     * @param password La password del bibliotecario
+     * @param e_mail L'email del bibliotecariə
+     * @param password La password del bibliotecariə
      */
     public Bibliotecario(String e_mail, String password) {
         this.e_mail = e_mail;
@@ -32,8 +27,10 @@ public class Bibliotecario {
     }
     /**
      * @brief Funzione che verifica gli attrubuti dell'oggetto Bibliotecario con i dati presenti nel database
+     * @brief Login da parte dei bibliotecari per accedere alle funzionalità ristrette.
+     * @post Il bibliotecariə ha accesso a tutte le funzionalità
      */
     public void loginBibliotecario() {};
-        
-  
+
+
 }
