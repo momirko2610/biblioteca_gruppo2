cc893f9a37866995bf2a3e156f2333e185519c05
Mirko Montella <m.montella25@studenti.unisa.it>
2025-12-08 18:38:35 +0100

    Aggiunti commenti per doxygen

--- src/main/java/Biblioteca/Bibliotecario.java
+++ src/main/java/Biblioteca/Bibliotecario.java
@@ -4,30 +4,34 @@
  * and open the template in the editor.
  */
 package Biblioteca;
-
+    /**
+     * @brief Classe che gestisce il login dei bibliotecari
+     */
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /**
- *
- * @author ssabr
+ * @author Mirko Montella
+ * @author Ciro Senese
+ * @author Achille Romano
+ * @author Sabrina Soriano
  */
 public class Bibliotecario {
-    private String e_mail;
-    private String password;
+    private String e_mail; /*!<Email dell'account letta da una textbox che verrà confrontata con i dati presenti nel database dal metodo loginBibliotecario*/
+    private String password; /*!<password dell'account letta da una textbox che verrà confrontata con i dati presenti nel database dal metodo loginBibliotecario*/
     
-    private static final String NAME = "database.json";
+    private static final String NAME = "database.json"; /*!<Nome del database da cui verranno confrontati i dati/*
     
     private static final Gson database = new GsonBuilder().setPrettyPrinting().create();
     /**
-     * @brief 
+     * @brief Costruttore di base
      */
     public Bibliotecario(String e_mail, String password) {
         this.e_mail = e_mail;
         this.password = password;
     }
     /**
-     * @brief 
+     * @brief Funzione che verifica gli attrubuti dell'oggetto Bibliotecario con i dati presenti nel database
      */
     public void loginBibliotecario() {};
         
